6b91e60dc33fdd10185beb7e9e1d42163bd39c6e
Siripong Thieanchumphan <aeio.yu@gmail.com>
2022-05-24 14:46:04 +0700

    remove unused libs

--- src/libs/matterport.tsx
+++
@@ -1,8 +0,0 @@
-import { configs } from "@/configs/configs";
-import { ShowcaseEmbedWindow } from "matterport";
-
-export const initMatterport = async (iframe: HTMLIFrameElement) => {
-  const embeddingWindow = window as ShowcaseEmbedWindow;
-  const mpSdk = await embeddingWindow.MP_SDK.connect(iframe, configs.matterport.key, "");
-  return mpSdk;
-};
